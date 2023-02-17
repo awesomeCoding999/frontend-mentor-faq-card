ebe1a8ebb86590299ba725752ab4087893a3797a
CodingChick <awesomecoding26@gmail.com>
2023-02-16 23:28:58 -0800

    feat: adding rest of mobile styles

--- src/components/faq-card/FAQCard.tsx
+++ src/components/faq-card/FAQCard.tsx
@@ -29,12 +29,18 @@ export const FAQCard = () => {
         />
       </picture>
       <div className="card-container">
-        <h1>FAQ</h1>
-        <ul>
+        <h1 className="title">FAQ</h1>
+        <ul className="faq-items">
           {faqList.map(({ id, question, answer }) => (
             <li className="faq" key={id}>
               <div className="question-container">
-                <p>{question}</p>
+                <p
+                  className={`question ${
+                    activeFAQ === id ? "active-question" : ""
+                  }`}
+                >
+                  {question}
+                </p>
                 <button className="arrow-btn" id={id} onClick={toggleAnswer}>
                   <img
                     className={`${activeFAQ === id ? "rotate-icon-up" : ""}`}
@@ -44,13 +50,13 @@ export const FAQCard = () => {
                 </button>
               </div>
               <p
-                className={`${
-                  activeFAQ === id ? "show-answer" : "hide-answer"
-                }`}
+                className={`
+                  ${activeFAQ === id ? "show" : "hide"}-answer
+               `}
               >
                 {answer}
               </p>
-              <hr />
+              <hr className="divider" />
             </li>
           ))}
         </ul>
